6f3969d0a0171faa9297a11d03218e4498b07a4b
Sean Bhupathi <seanbhupathi@gmail.com>
2017-02-08 13:14:26 -0500

    installed mysql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'EVIL or NOT'});
+});
+
+router.get('/standings', (req,res,next)=>{
+    // res.send("plop");
+    res.render('standings', {});
 });
 
 module.exports = router;
